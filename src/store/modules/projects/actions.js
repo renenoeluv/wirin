5c779e04e779147f7011394d7e52c80cb83a075d
jgonzalez-m <jorgej.gonzalezmartinez@gmail.com>
2021-04-25 15:14:23 -0400

    dependencias arregladas y otros bugs

--- src/store/modules/projects/actions.js
+++ src/store/modules/projects/actions.js
@@ -15,7 +15,7 @@ export async function fetchProjects({commit}){
 export async function CreateProject({commit},data){
     
     try {
-        console.log("obteniendo proyectos");
+        console.log("ceando proyecto");
         data = await JSON.stringify(data)
         //data = await JSON.parse(data)
         console.log(data)
@@ -23,6 +23,34 @@ export async function CreateProject({commit},data){
         console.log(data)
         let res = await fetch("http://localhost:3000/proyecto",{
             method: 'POST',
+            headers: {
+                        'Content-Type': 'application/json'
+                        //'Content-Type': 'application/x-www-form-urlencoded',
+                      },
+                      
+            body:data
+            
+        });
+        let resMdd = await res.json();
+        commit ('projects/addProject',resMdd,{root:true})
+        
+    } catch {
+        console.log("hubo un error");
+        
+        return Error;
+    }
+}
+export async function UpdateProject({commit},data){
+    
+    try {
+        console.log("actualizando proyectos");
+        data = await JSON.stringify(data)
+        //data = await JSON.parse(data)
+        console.log(data)
+        //data = await JSON.stringify(data)
+        console.log(data)
+        let res = await fetch("http://localhost:3000/proyecto",{
+            method: 'PUT',
             headers: {
                         'Content-Type': 'application/json'
                         //'Content-Type': 'application/x-www-form-urlencoded',
